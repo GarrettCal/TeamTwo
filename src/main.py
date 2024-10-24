48d2576c841b4588c5f29ec43b75b36d36e968ea
calebclayson <clayson.caleb@gmail.com>
2024-10-24 15:15:34 -0600

    fixed it

--- src/main.py
+++ src/main.py
@@ -3,19 +3,22 @@ from taskmanager import TaskManager
 def main():
     task_manager = TaskManager()
     print("Welcome to the task management system")
-    print("Please choose an option: 1. Add Task 2. Remove Task 3. View Tasks 4. Exit")
-    choice = input()
-    if choice == '1':
-        task_manager.add_task()
-    elif choice == '2':
-        task_manager.remove_task()
-    elif choice == '3':
-        task_manager.view_tasks()
-    elif choice == '4':
-        exit()
-    else:
-        print("Invalid choice, please try again.")
-        main()
+    while True:
+        print("Please choose an option: 1. Add Task 2. Remove Task 3. View Tasks 4. Exit")
+        choice = input()
+        if choice == '1':
+            task = input("Enter the task to add: ")
+            task_manager.add_task(task)
+        elif choice == '2':
+            task = input("Enter the task to remove: ")
+            task_manager.remove_task(task)
+        elif choice == '3':
+            task_manager.view_tasks()
+        elif choice == '4':
+            exit()
+        else:
+            print("Invalid choice, please try again.")
+            
 
 if __name__ == "__main__":
     main()
