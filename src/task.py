650412c1e2b36e2b68cbdec78a2590410c684cf8
calebclayson <clayson.caleb@gmail.com>
2024-10-24 15:26:42 -0600

    added task class

---
+++ src/task.py
@@ -0,0 +1,11 @@
+class Task:
+    def __init__(self, name):
+        self.name = name
+        self.status = "Pending"
+
+    def edit_task(self, new_name):
+        pass
+
+    def mark_complete(self):
+        pass
+
